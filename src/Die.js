6f2e1bf483c6a2205cdc6a0d9a077603ecc596e8
asadazhari <asadkamal9@gmail.com>
2022-08-25 09:33:26 +0400

    Added additional features

--- src/Die.js
+++ src/Die.js
@@ -14,10 +14,13 @@ class Die extends Component {
     this.props.handleClick(this.props.idx);
   }
   render() {
-    const { wordNum, locked, val, disabled } = this.props;
+    const { wordNum, locked, val, disabled, rolling } = this.props;
     let classes = `Die fas fa-dice-${wordNum[val - 1]} fa-5x `;
     if (locked) {
-      classes += "Die-locked";
+      classes += "Die-locked ";
+    }
+    if (rolling) {
+      classes += "Die-rolling";
     }
 
     return (
--- src/Game.js
+++ src/Game.js
@@ -10,7 +10,9 @@ class Game extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dice: Array.from({ length: NUM_DICE }),
+      dice: Array.from({ length: NUM_DICE }).map(
+        (d) => Math.floor(Math.random() * 6) + 1
+      ),
       locked: Array(NUM_DICE).fill(false),
       rollsLeft: NUM_ROLLS,
       rolling: false,
@@ -30,9 +32,17 @@ class Game extends Component {
         chance: undefined,
       },
     };
-    this.roll = this.roll.bind(this);
+    this.animateRoll = this.animateRoll.bind(this);
     this.doScore = this.doScore.bind(this);
     this.toggleLocked = this.toggleLocked.bind(this);
+    this.roll = this.roll.bind(this);
+    this.displayRoll = this.displayRoll.bind(this);
+  }
+
+  animateRoll() {
+    this.setState({ rolling: true }, () => {
+      setTimeout(this.roll, 1000);
+    });
   }
 
   roll(evt) {
@@ -43,12 +53,13 @@ class Game extends Component {
       ),
       locked: st.rollsLeft > 1 ? st.locked : Array(NUM_DICE).fill(true),
       rollsLeft: st.rollsLeft - 1,
+      rolling: false,
     }));
   }
 
   toggleLocked(idx) {
     // toggle whether idx is in locked or not
-    if (this.state.rollsLeft > 0) {
+    if (this.state.rollsLeft > 0 && !this.state.rolling) {
       this.setState((st) => ({
         locked: [
           ...st.locked.slice(0, idx),
@@ -66,10 +77,26 @@ class Game extends Component {
       rollsLeft: NUM_ROLLS,
       locked: Array(NUM_DICE).fill(false),
     }));
-    this.roll();
+    this.animateRoll();
+  }
+
+  displayRoll() {
+    const messages = [
+      "0 Rolls left",
+      "1 Roll left",
+      "2 Rolls left",
+      "3 Rolls left",
+    ];
+
+    if (this.state.rolling) {
+      return "Please Wait...";
+    }
+
+    return messages[this.state.rollsLeft];
   }
 
   render() {
+    const { dice, locked, rollsLeft, rolling, scores } = this.state;
     return (
       <div className="Game">
         <header className="Game-header">
@@ -77,26 +104,24 @@ class Game extends Component {
 
           <section className="Game-dice-section">
             <Dice
-              dice={this.state.dice}
-              locked={this.state.locked}
+              dice={dice}
+              locked={locked}
               handleClick={this.toggleLocked}
-              disabled={this.state.rollsLeft === 0}
+              disabled={rollsLeft === 0}
+              rolling={rolling}
             />
             <div className="Game-button-wrapper">
               <button
                 className="Game-reroll"
-                disabled={
-                  this.state.locked.every((x) => x) ||
-                  this.state.rollsLeft === 0
-                }
-                onClick={this.roll}
+                disabled={locked.every((x) => x) || rollsLeft === 0 || rolling}
+                onClick={this.animateRoll}
               >
-                {this.state.rollsLeft} Rolls Left
+                {this.displayRoll()}
               </button>
             </div>
           </section>
         </header>
-        <ScoreTable doScore={this.doScore} scores={this.state.scores} />
+        <ScoreTable doScore={this.doScore} scores={scores} />
       </div>
     );
   }
